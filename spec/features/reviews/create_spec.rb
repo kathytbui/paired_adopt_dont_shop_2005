0e81eeae0d77e3591de05927fbb8d5f1ea348eba
“Neeru <neeram85@gmail.com>
2020-07-07 19:49:57 -0600

    User story 4 complete

--- spec/features/reviews/create_spec.rb
+++ spec/features/reviews/create_spec.rb
@@ -19,4 +19,22 @@ RSpec.describe "Create a new review" do
     expect(current_path).to eq("/shelters/#{cozy_kitten1.id}")
     expect(page).to have_content(title)
   end
+
+  it "can give a flash message if title, rating, and/or content isn't filled out in the form" do
+   cozy_kitten = Shelter.create(name: "Cozy Kitten Animal Shelter", address: "123 Maple Street", city: "Brooklyn", state: "NY", zip:12345)
+   title = "Review 1"
+   rating = 4
+   content = "It was wonder!"
+   picture = "image"
+
+   visit "/shelters/#{cozy_kitten.id}"
+
+   click_on "CREATE NEW REVIEW"
+   expect(current_path).to eq("/shelters/#{cozy_kitten.id}/reviews/new")
+   click_on "CREATE NEW REVIEW"
+
+   expect(page).to have_content("Error, you are missing information, Please fill out form completely")
+
+   expect(current_path).to eq("/shelters/#{cozy_kitten.id}/reviews/new")
+ end
 end
