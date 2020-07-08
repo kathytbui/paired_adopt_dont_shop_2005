5f1726786b951c4353cdbea1ca65ce7692535eb3
Judith Pillado <judith.pillado@gmail.com>
2020-07-08 17:05:32 -0500

    Add shelter review without an image

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,5 @@ class Review < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :rating
   validates_presence_of :content
-  validates_presence_of :picture
   belongs_to :shelter
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,7 +3,6 @@ RSpec.describe Review, type: :model do
     it { should validate_presence_of :title }
     it { should validate_presence_of :rating }
     it { should validate_presence_of :content }
-		it { should validate_presence_of :picture }
 	end
 
 	describe "relationships" do
